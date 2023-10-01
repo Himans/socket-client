ff91aa79368e01d7c94e09f1f6fbeac8c9976f7f
Himans <Test110@yandex.ru>
2023-10-01 10:17:50 +0300

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,20 +1,47 @@
 import React from "react";
-
 import styles from "../styles/Messages.module.css";
 
 const Messages = ({ messages, name }) => {
+  const handleEdit = (messageId, newMessage) => {
+    // Update the message with the new content
+    // You can use the messageId to identify the message in the userMessages state
+    // and update the message content with the newMessage
+  };
+
+  const handleDelete = (messageId) => {
+    // Remove the message from the userMessages state
+    // You can use the messageId to identify the message and filter it out
+  };
+
   return (
     <div className={styles.messages}>
-      {messages.map(({ user, message }, i) => {
+      {messages.map(({ user, message, id }, i) => {
         const itsMe =
           user.name.trim().toLowerCase() === name.trim().toLowerCase();
         const className = itsMe ? styles.me : styles.user;
 
         return (
-          <div key={i} className={`${styles.message} ${className}`}>
+          <div key={id} className={`${styles.message} ${className}`}>
             <span className={styles.user}>{user.name}</span>
 
             <div className={styles.text}>{message}</div>
+
+            {itsMe && (
+              <div className={styles.actions}>
+                <button
+                  className={styles.editButton}
+                  onClick={() => handleEdit(id, message)}
+                >
+                  Edit
+                </button>
+                <button
+                  className={styles.deleteButton}
+                  onClick={() => handleDelete(id)}
+                >
+                  Delete
+                </button>
+              </div>
+            )}
           </div>
         );
       })}
@@ -23,3 +50,4 @@ const Messages = ({ messages, name }) => {
 };
 
 export default Messages;
+
